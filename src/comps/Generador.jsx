e484e017c5f5fe388c9029e635509ac4114c283c
jonatanc04 <jcorcolesg98@gmail.com>
2024-06-19 17:32:26 +0200

    arreglos de adaptación a pantallas

--- src/comps/Generador.jsx
+++ src/comps/Generador.jsx
@@ -2,16 +2,16 @@ import "../styles/Generador.scss";
 
 export default function GeneradorTool() {
   return (
-    <div className="gentool-body d-flex flex-column align-items-center col-12 mt-5">
-      <h3 className="mt-4">Generador Zentella 6.0 (DEMO)</h3>
+    <div className="gentool-body d-flex flex-column align-items-center col-12">
+      <h3 className="mt-4 text-center">Generador Zentella 6.0 (DEMO)</h3>
       <div className="col-12 d-flex flex-row align-items-center justify-content-center">
-        <img className="m-3" src="https://static.wikia.nocookie.net/inazuma-eleven-new-departure/images/d/d8/Raimon.webp" alt="error"></img>
-        <div className="d-flex flex-row align-items-center justify-content-center">Equipo Local</div>
+        <img className="m-3" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Escudo_vacío.png/647px-Escudo_vacío.png" alt="error"></img>
+        <div className="d-flex flex-row align-items-center justify-content-center text-center">Equipo Local</div>
         <h2 className="col-3 d-flex flex-row align-items-center justify-content-center">0-0</h2>
-        <div className="d-flex flex-row align-items-center justify-content-center">Equipo Visitante</div>
-        <img className="m-3" src="https://static.wikia.nocookie.net/inazuma-eleven-new-departure/images/d/d8/Raimon.webp" alt="error"></img>
+        <div className="d-flex flex-row align-items-center justify-content-center text-center">Equipo Visitante</div>
+        <img className="m-3" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Escudo_vacío.png/647px-Escudo_vacío.png" alt="error"></img>
       </div>
-      <div className="col-12 d-flex flex-row">
+      <div className="col-12 d-flex flex-row ">
         <form className="col-12 d-flex flex-column">
           <div className="col-12 d-flex flex-row">
             <div className="col-6 d-flex flex-column align-items-center">
@@ -23,7 +23,7 @@ export default function GeneradorTool() {
               <input type="file"></input>
             </div>
           </div>
-          <h5 className="col-12 mt-3 d-flex flex-column align-items-center">Opciones</h5>
+          <h5 className="col-12 mt-5 d-flex flex-column align-items-center">Opciones</h5>
           <div className="col-12 d-flex flex-row align-items-center justify-content-center">
             <div className="col-3">
               <label className="m-2">Activar buff de local</label>
--- src/pages/generador.jsx
+++ src/pages/generador.jsx
@@ -5,10 +5,10 @@ import GeneradorTool from '../comps/Generador';
 
 export default function Generador() {
   return (
-    <div className="generador-body container-fluid m-0 p-0">
+    <div className="generador-body container-fluid p-0">
       <div className='d-flex flex-row col-12'>
         <div className='image-container col-3 d-flex align-items-center justify-content-center'><img src={samguk} alt='error' /></div>
-        <div className='generator-container col-6 d-flex mt-5'>
+        <div className='generator-container col-6 d-flex'>
           <GeneradorTool />
         </div>
         <div className='image-container col-3 d-flex align-items-center justify-content-center'><img src={lanzer} alt='error' /></div>
